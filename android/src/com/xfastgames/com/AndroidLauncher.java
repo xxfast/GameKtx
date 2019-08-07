f4e2f9a4de8b0c10f9a0e93ccd2553a4198dcad3
xxfast <isuru@xfastgames.com>
2019-08-08 00:05:58 +1000

    Initial commit

---
+++ android/src/com/xfastgames/com/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.xfastgames.com;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.xfastgames.com.GameKt;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new GameKt(), config);
+	}
+}
