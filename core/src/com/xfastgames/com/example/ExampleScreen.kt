e39a8e060441222e65fcf49b48a3ee186ad77214
xxfast <isuru@xfastgames.com>
2019-08-31 22:06:42 +1000

    add hello world

--- core/src/com/xfastgames/com/example/ExampleScreen.kt
+++ core/src/com/xfastgames/com/example/ExampleScreen.kt
@@ -4,7 +4,7 @@ import com.badlogic.gdx.graphics.Color
 import com.badlogic.gdx.graphics.OrthographicCamera
 import com.badlogic.gdx.graphics.g2d.BitmapFont
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
-import com.badlogic.gdx.utils.viewport.StretchViewport
+import com.badlogic.gdx.utils.viewport.ScreenViewport
 import com.badlogic.gdx.utils.viewport.Viewport
 import ktx.app.KtxScreen
 import ktx.graphics.use
@@ -12,7 +12,7 @@ import ktx.graphics.use
 class ExampleScreen : KtxScreen {
   private val font = BitmapFont()
   private val camera: OrthographicCamera = OrthographicCamera()
-  private val viewport: Viewport = StretchViewport(800f, 400f, camera)
+  private val viewport: Viewport = ScreenViewport(camera)
 
   private val batch: SpriteBatch = SpriteBatch().apply {
     color = Color.WHITE
@@ -24,9 +24,8 @@ class ExampleScreen : KtxScreen {
 
   override fun render(delta: Float) {
     batch.projectionMatrix = camera.combined
-    camera.position.x += 10
     batch.use {
-      font.draw(it, "Hello World!", 10f, 10f)
+      font.draw(it, "Hello World!", camera.position.x, camera.position.y)
     }
   }
 
--- ios/src/com/xfastgames/com/IOSLauncher.kt
+++ ios/src/com/xfastgames/com/IOSLauncher.kt
@@ -1,15 +1,15 @@
 package com.xfastgames.com
 
-import org.robovm.apple.foundation.NSAutoreleasePool
-import org.robovm.apple.uikit.UIApplication
 import com.badlogic.gdx.backends.iosrobovm.IOSApplication
 import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration
-import com.xfastgames.com.GameKt
+import com.xfastgames.com.example.ExampleGame
+import org.robovm.apple.foundation.NSAutoreleasePool
+import org.robovm.apple.uikit.UIApplication
 
 class IOSLauncher : IOSApplication.Delegate() {
   override fun createApplication(): IOSApplication {
     val config = IOSApplicationConfiguration()
-    return IOSApplication(GameKt(), config)
+    return IOSApplication(ExampleGame(), config)
   }
 
   companion object {
